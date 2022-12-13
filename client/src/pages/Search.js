8caae595eea30a00c0d882508905e4b13f3f37bb
cmckee120993 <103902212+cmckee120993@users.noreply.github.com>
2022-12-13 06:26:54 +0100

    Update Search.js
    included api search and .env for key

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Card, Input} from 'semantic-ui-react';
+import axios from 'axios';
+
+
+
+
 
 function Search() {
-    return (
-        <h2>Search for Products Here</h2>
+    const [APIData, setAPIData] = useState([]);
+
+    
+    const searchItem = function() {
+    const searchParams =  document.querySelector('.search-words').value;
+    console.log(searchParams);
+    const options = {
+        method: 'POST',
+        url: 'https://mpowerapi.azurewebsites.net/api/v1/items/search',
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        },
+        data: {search: `${searchParams}`}
+      };
+      
+        axios.request(options).then(function (response) {
+        console.log(response.data.Results);
+        setAPIData(response.data.Results);
+      }).catch(function (error) {
+        console.error(error);
+      })
+    };
+
+      return (
+        <>
+            <input type="text" className="search-words" placeholder="Search for our products here..."></input>
+            <button type="submit" className="search-button" onClick={searchItem}>Search</button>
+            <Card.Group itemsPerRow={4}>
+                {APIData.map((item) => {
+                    return (
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>{item.Description}</Card.Header>
+                                <Card.Description>
+                                    {item.CaseRetail}
+                                    {item.QuantityAvailable}
+                                    {item.Category}
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    )
+                })}
+            </Card.Group>
+        </>
     );
+    
 };
 
 export default Search;
