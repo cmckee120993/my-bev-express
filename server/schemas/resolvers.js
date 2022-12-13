d0aa10c3712cd5baa16a617d0416962a757e5cd7
cmckee120993 <103902212+cmckee120993@users.noreply.github.com>
2022-12-13 06:17:16 +0100

    Update resolvers.js
    Created queries/mutations for front to back end integration

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,115 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Order, Product } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
 	Query: {
-		users: async () => {
-			return await User.find({}).select('-password');
+		orders: async () => {
+			return Order.find({});
 		},
-		me: async (parent, args, context) => {
+
+		order: async (parent, { _id }, context) => {
 			if (context.user) {
-				const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-
-				return userData;
-			}
-
-			throw new AuthenticationError('Not logged in');
+				const user = await User.findById(context.user._id).populate({
+					path: 'orders.products'
+				});
+				return user.orders.id(_id);
 		}
+		throw new AuthenticationError('Not logged in.')
+	}, 
+
+	users: async () => {
+		return await User.find()
+	},
+	
+	products: async (parent, args) => {
+		return Product.find()
+	},
+	
 	},
 
 	Mutation: {
-		addUser: async (_, args) => {
+		addUser: async (parent, args) => {
 			const user = await User.create(args);
 			const token = signToken(user);
 			
 			return { token, user };
-		}
+		},
+
+		updateUser: async (parent, args, context) => {
+			if (context.user) {
+				return await User.findByIdAndUpdate(context.user._id, args, {new: true});
+			}
+
+			throw new AuthenticationError('Not logged in');
+		},
+		
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError('Incorrect information');
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError('Incorrect information');
+			}
+
+			const token = signToken(user);
+
+			return { token, user };
+		},
+
+		addProduct: async (parent, args, context) => {
+			if(context.user) {
+				return Product.create(args);
+			}
+			throw new AuthenticationError('Not logged in');
+		},
+		
+		addOrder: async (parent, {orderOwner, orderDelivDate, products}, context) => {
+			if(context.user) {
+			const order = new Order({ products });
+			await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+			return order;
+			}
+
+		throw new AuthenticationError('Not logged in');
+		},
+
+		addToOrder: async (parent, {orderId, name, price, quantity}, context) => {
+			if(context.user) {
+				return Order.findOneAndUpdate(
+					{ _id: orderId },
+					{
+						$addToSet: {name,
+									price,
+									quantity,
+									}
+					},
+					{
+						new: true,
+						runValidators: true,
+					}
+				)
+			};
+
+			throw new AuthenticationError('Not logged in');
+		},
+
+		removeFromOrder: async (parent,{ orderId, productId }, context) => {
+			if(context.user) {
+				return Order.findOneAndUpdate(
+					{ _id: orderId},
+					{$pull: { products: { _id: productId } } },
+					{ new: true }
+				);
+			};
+
+			throw new AuthenticationError('Not logged in');
+		},
 	},
 };
 
