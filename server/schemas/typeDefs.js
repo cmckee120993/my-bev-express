05a9360e444b6ccd374c7cfa4a27467c33248df8
cmckee120993 <103902212+cmckee120993@users.noreply.github.com>
2022-12-13 06:17:45 +0100

    Update typeDefs.js
    Defined categories and queries/mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,22 +3,46 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 	type User {
 		_id: ID
-		username: String
+		firstName: String
+		lastName: String
 		email: String
+		orders: [Order]
+	}
+
+	type Product {
+	_id: ID
+	name: String
+	price: Int
+	quantity: Int
+	}
+
+	type Order {
+	_id: ID
+	purchaseDate: String
+	deliveryDate: String
+	products: [Product]
 	}
 
 	type Auth {
-		token: ID!
-		user: User
+	token: ID
+	user: User
 	}
 
 	type Query {
+		products: [Product]
+		orders: [Order]
+		order(_id: ID!): Order
 		users: [User]
-		me: User
 	}
 
 	type Mutation {
-		addUser(username: String!, email: String!, password: String!): Auth
+		addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+		addProduct(name: String!, price: Int!, quantity: Int!): Product
+		addOrder(orderOwner: String!, orderDelivDate: String!): Order
+		addToOrder(orderId: ID!, name: String!, price: Int!, quantity: Int!): User
+		updateUser(firstName: String, lastName: String, email: String, password: String): User
+		login(email: String!, password: String!): Auth
+		removeFromOrder(orderId: ID!, productId: ID!): Order
 	}
 `;
 
