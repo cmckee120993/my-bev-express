6b36f566a67a4f1b7a1d28b451684232c86fe8b8
cmckee120993 <103902212+cmckee120993@users.noreply.github.com>
2022-12-13 06:26:16 +0100

    Update Login.js
    connected login to gql and created html

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,72 @@
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 import '../styles/Login.css';
 
 
-function Login() {
+function Login(props) {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: {email: formState.email, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
     return (
-        <h1>
-            This is the login page!
-        </h1>
+        <>
+        <h2>Login</h2>
+        <Link to="/signup"> No account? Signup now!</Link>
+
+        <form onSubmit={handleFormSubmit}>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input 
+                placeholder="youremail@example.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="pwd">Password:</label>
+                <input 
+                placeholder="*****"
+                name="password"
+                type="password"
+                id="password"
+                onChange={handleChange}
+                />
+            </div>
+            {error ? (
+                <div>
+                    <p>The provided credentials are incorrect...</p>
+                </div>
+            ) : null}
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+        </>
     );
 };
 
