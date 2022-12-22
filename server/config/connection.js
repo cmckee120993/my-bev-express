03e3a6856a5282e0c8e087e4958241c98c6d18f9
Clayton McKee <cmckee120993@gmail.com>
2022-12-22 07:12:25 -0500

    changed server file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/my-bev-express', {
+mongoose.connect(process.env.MONGODB_URI {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
