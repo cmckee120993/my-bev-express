fb34947e3e8ca9ba1e3d48327bf4e63361891e91
Clayton McKee <cmckee120993@gmail.com>
2022-12-23 06:41:55 -0500

    change to connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bevexpress', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
